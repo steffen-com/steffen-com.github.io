fbaafe720671ec555a7e175d5e6eec309b004e18
steffen-com <e-steaga@iskule.no>
2022-06-08 14:22:41 +0200

    rt

---
+++ resources/javascript/string.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    let splitstring = str.split("rergerggerg");
+
+    let reversearray = splitstring.reverse();
+
+    let joinArray = reversearray.join("ergergerg");
+
+    return joinArray;
+
+    console.log()
+}
+reverseString("hello");
+
